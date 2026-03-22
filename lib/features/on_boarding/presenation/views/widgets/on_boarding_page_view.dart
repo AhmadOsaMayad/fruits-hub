47b39184ae0eaab5f2b93a922a3370b59e5921a3
AhmadOsaMayad <ao699168@gmail.com>
2026-03-23 00:13:58 +0300

    feat(onboarding): add new images and implement onboarding page view
    - Added new image assets for onboarding screens in app_images.dart.
    - Updated OnBoardingView to include SafeArea for better layout.
    - Enhanced OnBoardingPageView to utilize new PageViewItem widget for displaying onboarding content.
    - Created PageViewItem widget to encapsulate the layout and functionality of each onboarding page.

--- lib/features/on_boarding/presenation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presenation/views/widgets/on_boarding_page_view.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/utils/app_images.dart';
+import 'package:fruit_hub/features/on_boarding/presenation/views/widgets/page_view_item.dart';
 
 class OnBoardingPageView extends StatelessWidget {
   const OnBoardingPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PageView(children: [
+    return PageView(
+      children: [
+        PageViewItem(
+          image: Assets.imagesPageViewItem1Image,
+          backgroundImage: Assets.imagesPageViewItem1BackgroundImage,
+          subtitle:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          title: Row(
+            children: [Text('مرحبًا بك في'), Text('Fruit'), Text('HUB')],
+          ),
+        ),
       ],
     );
   }
