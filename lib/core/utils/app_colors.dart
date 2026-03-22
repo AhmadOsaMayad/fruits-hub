f5831b8ec9dbce284e71a24a3bc38d44434dc879
AhmadOsaMayad <ao699168@gmail.com>
2026-03-23 01:42:55 +0300

    feat: add AppColors utility and integrate DotsIndicator in OnBoardingViewBody

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF1F5E3B);
+  // static const Color secondary = Color(0xFF2196F3);
+  // static const Color background = Color(0xFFF5F5F5);
+  // static const Color text = Color(0xFF333333);
+  // static const Color hint = Color(0xFF999999);
+}
