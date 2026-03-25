afa14131530c02127b9420c56e789bc5cbf55c49
AhmadOsaMayad <ao699168@gmail.com>
2026-03-26 00:10:52 +0300

    feat: integrate Firebase with Flutter app and configure necessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/helpers/on_generate_routes.dart';
 import 'package:fruit_hub/core/services/shared_preference_singleton.dart';
 import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
+import 'package:fruit_hub/firebase_options.dart';
 import 'package:fruit_hub/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const FruitHub());
 }
 
